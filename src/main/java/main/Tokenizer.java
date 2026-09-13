20be290b1d56b1f377b1809f81df0465d1ab629a
Zeyad Abdelfattah <ziadabdelfattah68@gmail.com>
2026-09-13 10:22:16 +0300

    Add class-level documentation

--- src/main/java/main/Tokenizer.java
+++ src/main/java/main/Tokenizer.java
@@ -3,6 +3,11 @@ package main;
 import java.util.ArrayList;
 import java.util.List;
 
+/// Tokenizer class that provides a method to tokenize input strings into a list of tokens.
+///
+/// It uses a regular expression to split the input string into tokens, removing any non-alphanumeric characters (except for apostrophes) and converting all tokens to lowercase.
+///
+/// It also removes any empty tokens from the final list.
 public class Tokenizer {
     private static final String TOKEN_REGEX = "[^a-z0-9']+";
 
