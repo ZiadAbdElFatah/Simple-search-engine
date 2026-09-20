986352c4658d3f349d56a22d6d6441d0b153f141
Zeyad Abdelfattah <ziadabdelfattah68@gmail.com>
2026-09-20 20:08:54 +0300

    Add operators

--- src/main/java/main/QueryParser.java
+++ src/main/java/main/QueryParser.java
@@ -10,22 +10,29 @@ public class QueryParser {
         List<String> tokens = new ArrayList<>();
         boolean inQuotes = false;
         for (String token : input.split(TOKEN_REGEX)) {
-            if (token.startsWith("\"") && token.endsWith("\"") && token.length() > 1) {
-                tokens.add(token.substring(1, token.length() - 1));
-                continue;
-            } else if (token.endsWith("\"")) {
-                tokens.add(token.substring(0, token.length() - 1));
-                inQuotes = false;
-                continue;
-            } else if (token.startsWith("\"")) {
-                tokens.add(token.substring(1));
-                inQuotes = true;
-                continue;
-            }
-            if (inQuotes) {
-                tokens.add(token);
-            } else {
-                tokens.add(token.toLowerCase());
+            switch (token) {
+                case "AND" -> tokens.add("AND");
+                case "OR" -> tokens.add("OR");
+                case "NOT" -> tokens.add("NOT");
+                default -> {
+                    if (token.startsWith("\"") && token.endsWith("\"") && token.length() > 1) {
+                        tokens.add(token.substring(1, token.length() - 1));
+                        continue;
+                    } else if (token.endsWith("\"")) {
+                        tokens.add(token.substring(0, token.length() - 1));
+                        inQuotes = false;
+                        continue;
+                    } else if (token.startsWith("\"")) {
+                        tokens.add(token.substring(1));
+                        inQuotes = true;
+                        continue;
+                    }
+                    if (inQuotes) {
+                        tokens.add(token);
+                    } else {
+                        tokens.add(token.toLowerCase());
+                    }
+                }
             }
         }
         return tokens;
